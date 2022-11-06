9a6b7a6544429cc7e5871f79c13ccb5b1052a88c
Metody Aleksov <teddy57603@gmail.com>
2022-11-06 11:31:33 +0200

    deserializes json file

--- PcConfigurator/Program.cs
+++ PcConfigurator/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var jsonText = await File.ReadAllTextAsync("json/pc-store-inventory.json");
+var json = JsonSerializer.Deserialize<Data>(jsonText);
