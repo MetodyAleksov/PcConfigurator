d4c170512003818062a920eda7bf40b87fd2aba3
Metody Aleksov <teddy57603@gmail.com>
2022-11-07 11:16:13 +0200

    added cpu model

--- PcConfigurator/Models/CPU.cs
+++ PcConfigurator/Models/CPU.cs
@@ -11,4 +11,15 @@
     public string Socket { get; set; }
 
     public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"Type: {this.ComponentType}\n" +
+            $"Part number: {this.PartNumber}\n" +
+            $"Name: {this.Name}\n" +
+            $"Supported memory: {this.SupportedMemory}\n" +
+            $"Socket: {this.Socket}," +
+            $"Price: {this.Price}\n" +
+            $"---------";
+    }
 }
