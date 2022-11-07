d587f35c0ce87e60e7a1cde12285ee31069f0970
Metody Aleksov <teddy57603@gmail.com>
2022-11-07 11:16:26 +0200

    added motherboard model

--- PcConfigurator/Models/Motherboard.cs
+++ PcConfigurator/Models/Motherboard.cs
@@ -9,4 +9,14 @@
     public string Socket { get; set; }
 
     public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"Type: {this.ComponentType}\n" +
+            $"Part number: {this.PartNumber}\n" +
+            $"Name: {this.Name}\n" +
+            $"Socket: {this.Socket}," +
+            $"Price: {this.Price}\n" +
+            $"---------";
+    }
 }
